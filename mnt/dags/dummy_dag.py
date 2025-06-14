6858616dd7a971c9e641466143c4422a89321aaf
mandhani-ujjawal <ujjawal.mandhani@lumiq.ai>
2025-06-14 17:57:02 +0530

    fixing docker-compose airflow.cfg should be same for every instance

--- mnt/dags/dummy_dag.py
+++ mnt/dags/dummy_dag.py
@@ -7,13 +7,12 @@ from airflow import DAG
 from airflow.sdk import Variable
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator as DummyOperator
+import json
 
-
- 
  
 def foo_func():
-    print(Variable.get("uat_project_id"))
-    return Variable.get("uat_project_id")
+    print(json.loads(Variable.get("uat_project_id")))
+    return json.loads(Variable.get("uat_project_id"))
 
  
 default_args = {
@@ -23,7 +22,10 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
  
- 
+def my_function(xcom_value, **kwargs):
+    print(":::::::::::::::", xcom_value.replace('\\\\\\\\', '/'))
+    print(f"Pulled XCom value via template: {xcom_value}")
+
 with DAG(
     dag_id = 'foo_example',
     default_args=default_args,
@@ -35,10 +37,16 @@ with DAG(
     start_task = DummyOperator(task_id='start')
 
     dummy_task = PythonOperator(
-        task_id=f"{Variable.get("uat_project_id")}",
+        task_id="some_task",
         python_callable=foo_func,
     )
- 
+    
+    pull_task = PythonOperator(
+        task_id='pull_task',
+        python_callable=my_function,
+        op_args=["{{ ti.xcom_pull(task_ids='some_task') }}"],
+        dag=dag,
+    )
     end = DummyOperator(task_id='end')
  
-    start_task >> dummy_task >> end
+    start_task >> dummy_task >> pull_task >> end
